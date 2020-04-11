5a27808e51726c2ef6e95302baa9ae4689d13b0d
Julian Brown <jbrown@yext.com>
2020-04-11 20:15:31 +0100

    source in bash profile if not already sourced

--- install.sh
+++ install.sh
@@ -3,16 +3,14 @@
 # Install nvm to manage node versions
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-if [[ -d ~/.anvs ]]
-then
-    cd ~ && rm -rf .anvs
-    mkdir .anvs && cd .anvs
-    git clone git@github.com:bjulian5/anvs.git .
-    echo "source ~/.anvs/anvs.sh" >> ~/.bash_profile
-else
-    cd ~ && rm -rf .anvs
-    mkdir .anvs && cd .anvs
-    git clone git@github.com:bjulian5/anvs.git .
+cd ~ && rm -rf .anvs
+mkdir .anvs && cd .anvs
+git clone git@github.com:bjulian5/anvs.git .
+
+ALREADY_SOURCED=$(cat ~/.bash_profile | grep -o 'source ~/.anvs/anvs.sh')
+
+if [[ -z "$ALREADY_SOURCED" ]]; then
+  echo "source ~/.anvs/anvs.sh" >> ~/.bash_profile
 fi
 
 source ~/.bash_profile
